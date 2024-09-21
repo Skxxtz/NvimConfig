f1e3a8e811ba6dfae5a58ea524103463c8adc5da
Sebastian Kootz <skxxtz@gmail.com>
2024-09-21 20:27:30 +0200

    implemented new themes

--- lua/core/plugin_config/colorscheme.lua
+++ lua/core/plugin_config/colorscheme.lua
@@ -1,13 +1,18 @@
+-- CATPPUCCIN THEME: https://github.com/catppuccin/nvim
+-- VARIANTS: latte, frappe, macchiato, mocha
 require("catppuccin").setup({
     flavour = "mocha",
     transparent_background = true,
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = { "italic" }, -- Change the style of comments
+    styles = {
+        comments = { "italic" },
     },
 })
 table.insert(THEMES, "catppuccin")
 REVERSE_THEME["catppuccin"] = #THEMES
 
+
+-- ROSE PINE THEME: https://github.com/rose-pine/neovim
+-- VARIANTS: dawn, moon, auto, 
 require("rose-pine").setup({
     variant = "auto",
     dark_variant = "main",
@@ -19,6 +24,9 @@ require("rose-pine").setup({
 table.insert(THEMES, "rose-pine")
 REVERSE_THEME["rose-pine"] = #THEMES
 
+
+--  NIGHTFOX THEME: https://github.com/EdenEast/nightfox.nvim
+--  VARIANTS: night, day, dawn, dusk, carbon
 require("nightfox").setup({
     options = {
         transparent = true,
@@ -27,8 +35,11 @@ require("nightfox").setup({
 table.insert(THEMES, "nightfox")
 REVERSE_THEME["nightfox"] = #THEMES
 
+table.insert(THEMES, "carbonfox")
+REVERSE_THEME["carbonfox"] = #THEMES
 
-
-
+-- WHITEOUT THEME:
+table.insert(THEMES, "whiteout")
+REVERSE_THEME["whiteout"] = #THEMES
 
 
