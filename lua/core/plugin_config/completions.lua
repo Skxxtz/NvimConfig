8a3ae8329f696db0654c7a62c00ec49c98e830fb
Sebastian Kootz <skxxtz@gmail.com>
2024-09-21 17:54:41 +0200

    added auto parentheses on completion

--- lua/core/plugin_config/completions.lua
+++ lua/core/plugin_config/completions.lua
@@ -1,35 +1,42 @@
 local cmp = require("cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
 require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup({
-  completion = {
-    completeopt = 'menu,menuone,noinsert',
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-o>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-  }, {
-    { name = 'buffer' },
-  }),
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-o>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+    }, {
+        { name = 'buffer' },
+    }),
+
 })
 
+cmp.event:on(
+   "confirm_done",
+   cmp_autopairs.on_confirm_done()
+)
 
 
 
