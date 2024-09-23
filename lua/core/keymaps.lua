ba36b5e7ada22bea359fbd20b46f213473d53865
Sebastian Kootz <skxxtz@gmail.com>
2024-09-23 17:58:34 +0200

    clears terminal before printign, so that all messages are showing

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -44,6 +44,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Git keybind
 vim.keymap.set("n", "<leader>gc", function()
+    ClearTerm()
     vim.fn.system("git add .")
     local commit = vim.fn.input("commit message: ")
     local output = vim.fn.system(string.format('git commit -m "%s"', commit))
@@ -51,6 +52,7 @@ vim.keymap.set("n", "<leader>gc", function()
 end, { silent = true })
 
 vim.keymap.set("n", "<leader>gp", function()
+    ClearTerm()
     local output = vim.fn.system("git push");
     PrintOutput(output)
 end, { silent = true })
