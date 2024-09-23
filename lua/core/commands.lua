444429cf6dc7eac1084f841c449c62cbf65601b3
Sebastian Kootz <skxxtz@gmail.com>
2024-09-23 18:16:31 +0200

    test

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -26,6 +26,7 @@ function WriteTheme(index)
 end
 
 function PrintOutput(output, time)
+    AddOrReplaceTimer(10, ClearTerm)
     local lines = vim.split(output, "\n")
     for _,line in ipairs(lines) do
       print(line)
