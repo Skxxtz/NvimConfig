b2c1e8960c38a1795447bad2e0e163245bebbd3e
Sebastian Kootz <skxxtz@gmail.com>
2024-09-26 17:04:31 +0200

    improved structure and removed autocmds after closing

---
+++ lua/custom_plugins/command-palette/helpers.lua
@@ -0,0 +1,176 @@
+local maps = require("custom_plugins.command-palette.maps")
+local M = {}
+
+function M.AttatchEvents(prompt_buf, prompt_win, result_buf, result_win, paddings)
+    vim.api.nvim_create_augroup("CmdPalletteGroup", { clear = true })
+    vim.api.nvim_create_autocmd("TextChangedI", {
+        buffer = prompt_buf,
+        group = "CmdPalletteGroup",
+        callback = function()
+            local search_term = vim.api.nvim_buf_get_lines(prompt_buf, 0, -1, false)
+            M.Draw(search_term, result_buf, paddings)
+        end
+    })
+    vim.api.nvim_create_autocmd("WinResized", {
+        group = "CmdPalletteGroup",
+        callback = function ()
+            M.ApplyDims(result_win, "Results")
+            M.ApplyDims(prompt_win, "Command")
+        end
+    })
+
+    vim.api.nvim_create_autocmd("WinClosed", {
+        group = "CmdPalletteGroup",
+        callback = function(args)
+            local win_id = tonumber(args.match)
+            if win_id == result_win then
+                vim.api.nvim_win_close(prompt_win, false)
+                vim.api.nvim_del_augroup_by_name("CmdPalletteGroup")
+            elseif win_id == prompt_win then
+                vim.api.nvim_win_close(result_win, false)
+                vim.api.nvim_del_augroup_by_name("CmdPalletteGroup")
+            end
+        end
+    })
+end
+
+function M.AttatchBinds(prompt_buf, result_buf)
+    vim.api.nvim_buf_set_keymap(prompt_buf, "n", "q", ":q<CR>", { silent = true })
+    vim.api.nvim_buf_set_keymap(result_buf, "n", "q", ":q<CR>", { silent = true })
+    vim.api.nvim_buf_set_keymap(prompt_buf, "n", "<Esc>", ":q<CR>", { silent = true })
+    vim.api.nvim_buf_set_keymap(result_buf, "n", "<Esc>", ":q<CR>", { silent = true })
+end
+
+function M.OpenWindow(win_type)
+    local buf = vim.api.nvim_create_buf(false, true)
+    local width, height, left, top = M.GetDims(win_type)
+
+    local win = vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        style = "minimal",
+        width = width,
+        height = height,
+        col = left,
+        row = top,
+        border = "rounded",
+        title = win_type,
+        title_pos = "center",
+    })
+    vim.api.nvim_win_set_option(win, "winhighlight", "NormalFloat:Normal")
+    return buf, win, width
+end
+
+function M.GetDims(win_type)
+    local lines = vim.opt.lines:get()
+    local cols = vim.opt.columns:get()
+    local width = math.ceil(cols * 0.8)
+    local left = math.ceil((cols - width) * 0.5)
+    local height, top
+    if win_type == "Results" then
+        height = math.ceil(lines * 0.7 - 4)
+        top = 4
+    else
+        height = 1
+        top = math.ceil((lines * 0.75))
+    end
+    return width, height, left, top
+end
+
+function M.ApplyDims(win, win_type)
+    local width, height, left, top = M.GetDims(win_type)
+    vim.api.nvim_win_set_config(win, {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = left,
+        row = top,
+    })
+end
+
+function M.format(results, padding)
+    local lines = {}
+    local syntax_cols = {}
+    local p = padding
+    local x_offset = p.left_pad + p.mode_max + p.mod_max + p.bind_max
+    if #results > 0 then
+        for _, row in ipairs(results) do
+            local left_padding = string.rep(" ", p.left_pad)
+            local mode = row.mode .. string.rep(" ", p.mode_max - #row.mode)
+            local mod = row.mod .. string.rep(" ", p.mod_max - #row.mod)
+            local bind = row.bind .. string.rep(" ", p.bind_max - vim.fn.strwidth(row.bind))
+            local expl = row.explanation .. string.rep(" ", p.expl_max - #row.explanation)
+            local new_indices = {}
+            for _, index in ipairs(row.indices) do
+                table.insert(new_indices, index + x_offset)
+            end
+            table.insert(syntax_cols, new_indices)
+            table.insert(lines, left_padding .. mode .. mod .. bind .. expl)
+        end
+    end
+    return lines, syntax_cols
+end
+
+function M.Highlight(result_buf, cols)
+    if cols then
+        vim.api.nvim_set_hl(0, "Green", { fg = UserSettings.CommandPallettePlugin.AccentColor })
+        for i, row in ipairs(cols) do
+            for _, col in ipairs(row) do
+                vim.api.nvim_buf_add_highlight(result_buf, 0, "Green", i - 1, col - 1, col)
+            end
+        end
+    end
+end
+
+function M.GetPaddings(width)
+    local keymaps = maps.keymaps
+    local modes = {}
+    local mods = {}
+    local binds = {}
+    local explanations = {}
+    for _, row in ipairs(keymaps) do
+        table.insert(modes, vim.fn.strwidth(row.mode))
+        table.insert(mods, vim.fn.strwidth(row.mod))
+        table.insert(binds, vim.fn.strwidth(row.bind))
+        table.insert(explanations, vim.fn.strwidth(row.explanation))
+    end
+    local left_pad = 2
+    local mode_max = math.max(unpack(modes))
+    local mod_max = math.max(unpack(mods))
+    local bind_max = math.max(unpack(binds))
+    local expl_max = math.max(unpack(explanations))
+    local right_pad = 2
+
+    local total_width = left_pad + mode_max + mod_max + bind_max + expl_max + right_pad
+    if total_width + 20 < width then
+        mode_max = mode_max + 5
+        mod_max = mod_max + 5
+        bind_max = bind_max + 10
+    else
+        local diff = width - total_width
+        mode_max = mode_max + math.floor(diff * 0.25)
+        mod_max = mod_max + math.floor(diff * 0.25)
+        bind_max = bind_max + math.floor(diff *0.5)
+    end
+    return { left_pad=left_pad, mode_max = mode_max, mod_max = mod_max, bind_max = bind_max, expl_max = expl_max, right_pad=right_pad }
+end
+
+function M.Draw(search_term, result_buf, paddings)
+    local search_for = ""
+    for _, line in ipairs(search_term) do
+        if line then
+            search_for = line
+        end
+    end
+
+    local results = CmdPallette.fzf(maps.keymaps, search_for)
+    local lines, cols = { "" }, {}
+    if results then
+        lines, cols = M.format(results, paddings)
+    end
+    vim.bo[result_buf].modifiable = true
+    vim.api.nvim_buf_set_lines(result_buf, 0, -1, false, lines)
+    vim.bo[result_buf].modifiable = false
+    M.Highlight(result_buf, cols)
+end
+
+return M
--- lua/custom_plugins/command-palette/plugin.lua
+++ lua/custom_plugins/command-palette/plugin.lua
@@ -1,8 +1,5 @@
-local maps = require("custom_plugins.command-palette.maps")
-
-
+local helpers = require("custom_plugins.command-palette.helpers")
 CmdPallette = CmdPallette or {}
-CmdPalletteHelpers = CmdPalletteHelpers or {}
 
 function CmdPallette:new()
     local instance = {}
@@ -68,9 +65,9 @@ end
 
 function CmdPallette:Show()
     local editor_win = vim.api.nvim_get_current_win()
-    local result_buf, result_win, result_wid = CmdPalletteHelpers.OpenWindow("Results")
-    local prompt_buf, prompt_win, _ = CmdPalletteHelpers.OpenWindow("Command")
-    local paddings = CmdPalletteHelpers.GetPaddings(result_wid)
+    local result_buf, result_win, result_wid = helpers.OpenWindow("Results")
+    local prompt_buf, prompt_win, _ = helpers.OpenWindow("Command")
+    local paddings = helpers.GetPaddings(result_wid)
 
     vim.bo[result_buf].buftype = "nofile"
     vim.bo[result_buf].bufhidden = "wipe"
@@ -79,180 +76,11 @@ function CmdPallette:Show()
     vim.bo[result_buf].modifiable = true
     vim.api.nvim_feedkeys("i", "n", false)
 
-    CmdPalletteHelpers.Draw({""}, result_buf, paddings)
-    CmdPalletteHelpers.AttatchBinds(prompt_buf, result_buf)
-    CmdPalletteHelpers.AttatchEvents(prompt_buf, prompt_win, result_buf, result_win, paddings)
+    helpers.Draw({""}, result_buf, paddings)
+    helpers.AttatchBinds(prompt_buf, result_buf)
+    helpers.AttatchEvents(prompt_buf, prompt_win, result_buf, result_win, paddings)
 end
 
-function CmdPalletteHelpers.AttatchEvents(prompt_buf, prompt_win, result_buf, result_win, paddings)
-    vim.api.nvim_create_autocmd("TextChangedI", {
-        buffer = prompt_buf,
-        callback = function()
-            local search_term = vim.api.nvim_buf_get_lines(prompt_buf, 0, -1, false)
-            CmdPalletteHelpers.Draw(search_term, result_buf, paddings)
-        end
-    })
-    vim.api.nvim_create_autocmd("WinResized", {
-        callback = function ()
-            CmdPalletteHelpers.ApplyDims(result_win, "Results")
-            CmdPalletteHelpers.ApplyDims(prompt_win, "Command")
-        end
-    })
-
-    vim.api.nvim_create_autocmd("WinClosed", {
-        callback = function(args)
-            local win_id = tonumber(args.match)
-            if win_id == result_win then
-                vim.api.nvim_win_close(prompt_win, false)
-
-            elseif win_id == prompt_win then
-                vim.api.nvim_win_close(result_win, false)
-            end
-
-        end
-    })
-end
-
-function CmdPalletteHelpers.AttatchBinds(prompt_buf, result_buf)
-    vim.api.nvim_buf_set_keymap(prompt_buf, "n", "q", ":q<CR>", { silent = true })
-    vim.api.nvim_buf_set_keymap(result_buf, "n", "q", ":q<CR>", { silent = true })
-    vim.api.nvim_buf_set_keymap(prompt_buf, "n", "<Esc>", ":q<CR>", { silent = true })
-    vim.api.nvim_buf_set_keymap(result_buf, "n", "<Esc>", ":q<CR>", { silent = true })
-end
-
-
-
-function CmdPalletteHelpers.OpenWindow(win_type)
-    local buf = vim.api.nvim_create_buf(false, true)
-    local width, height, left, top = CmdPalletteHelpers.GetDims(win_type)
-
-    local win = vim.api.nvim_open_win(buf, true, {
-        relative = "editor",
-        style = "minimal",
-        width = width,
-        height = height,
-        col = left,
-        row = top,
-        border = "rounded",
-        title = win_type,
-        title_pos = "center",
-    })
-    vim.api.nvim_win_set_option(win, "winhighlight", "NormalFloat:Normal")
-    return buf, win, width
-end
-
-function CmdPalletteHelpers.GetDims(win_type)
-    local lines = vim.opt.lines:get()
-    local cols = vim.opt.columns:get()
-    local width = math.ceil(cols * 0.8)
-    local left = math.ceil((cols - width) * 0.5)
-    local height, top
-    if win_type == "Results" then
-        height = math.ceil(lines * 0.7 - 4)
-        top = 4
-    else
-        height = 1
-        top = math.ceil((lines * 0.75))
-    end
-    return width, height, left, top
-end
-
-function CmdPalletteHelpers.ApplyDims(win, win_type)
-    local width, height, left, top = CmdPalletteHelpers.GetDims(win_type)
-    vim.api.nvim_win_set_config(win, {
-        relative = "editor",
-        width = width,
-        height = height,
-        col = left,
-        row = top,
-    })
-end
-
-function CmdPalletteHelpers.format(results, padding)
-    local lines = {}
-    local syntax_cols = {}
-    local p = padding
-    local x_offset = p.left_pad + p.mode_max + p.mod_max + p.bind_max
-    if #results > 0 then
-        for _, row in ipairs(results) do
-            local left_padding = string.rep(" ", p.left_pad)
-            local mode = row.mode .. string.rep(" ", p.mode_max - #row.mode)
-            local mod = row.mod .. string.rep(" ", p.mod_max - #row.mod)
-            local bind = row.bind .. string.rep(" ", p.bind_max - vim.fn.strwidth(row.bind))
-            local expl = row.explanation .. string.rep(" ", p.expl_max - #row.explanation)
-            local new_indices = {}
-            for _, index in ipairs(row.indices) do
-                table.insert(new_indices, index + x_offset)
-            end
-            table.insert(syntax_cols, new_indices)
-            table.insert(lines, left_padding .. mode .. mod .. bind .. expl)
-        end
-    end
-    return lines, syntax_cols
-end
-
-function CmdPalletteHelpers.Highlight(result_buf, cols)
-    if cols then
-        vim.api.nvim_set_hl(0, "Green", { fg = UserSettings.CommandPallettePlugin.AccentColor })
-        for i, row in ipairs(cols) do
-            for _, col in ipairs(row) do
-                vim.api.nvim_buf_add_highlight(result_buf, 0, "Green", i - 1, col - 1, col)
-            end
-        end
-    end
-end
-
-function CmdPalletteHelpers.GetPaddings(width)
-    local keymaps = maps.keymaps
-    local modes = {}
-    local mods = {}
-    local binds = {}
-    local explanations = {}
-    for _, row in ipairs(keymaps) do
-        table.insert(modes, vim.fn.strwidth(row.mode))
-        table.insert(mods, vim.fn.strwidth(row.mod))
-        table.insert(binds, vim.fn.strwidth(row.bind))
-        table.insert(explanations, vim.fn.strwidth(row.explanation))
-    end
-    local left_pad = 2
-    local mode_max = math.max(unpack(modes))
-    local mod_max = math.max(unpack(mods))
-    local bind_max = math.max(unpack(binds))
-    local expl_max = math.max(unpack(explanations))
-    local right_pad = 2
-
-    local total_width = left_pad + mode_max + mod_max + bind_max + expl_max + right_pad
-    if total_width + 20 < width then
-        mode_max = mode_max + 5
-        mod_max = mod_max + 5
-        bind_max = bind_max + 10
-    else
-        local diff = width - total_width
-        mode_max = mode_max + math.floor(diff * 0.25)
-        mod_max = mod_max + math.floor(diff * 0.25)
-        bind_max = bind_max + math.floor(diff *0.5)
-    end
-    return { left_pad=left_pad, mode_max = mode_max, mod_max = mod_max, bind_max = bind_max, expl_max = expl_max, right_pad=right_pad }
-end
-
-function CmdPalletteHelpers.Draw(search_term, result_buf, paddings)
-    local search_for = ""
-    for _, line in ipairs(search_term) do
-        if line then
-            search_for = line
-        end
-    end
-
-    local results = CmdPallette.fzf(maps.keymaps, search_for)
-    local lines, cols = { "" }, {}
-    if results then
-        lines, cols = CmdPalletteHelpers.format(results, paddings)
-    end
-    vim.bo[result_buf].modifiable = true
-    vim.api.nvim_buf_set_lines(result_buf, 0, -1, false, lines)
-    vim.bo[result_buf].modifiable = false
-    CmdPalletteHelpers.Highlight(result_buf, cols)
-end
 
 
 local _ = CmdPallette:new()
